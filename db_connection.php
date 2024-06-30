df29721dd9a8bac4f66c46f6d0a4cde8a92a6eb3
Michael <28543330+Schello805@users.noreply.github.com>
2024-06-30 10:57:31 +0200

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,1 +1,17 @@
+<?php
+// db_connection.php
 
+$servername = "localhost"; // Datenbankserver
+$username = "haustieruser"; // Dein Datenbankbenutzername
+$password = "sicheres_passwort"; // Dein Datenbankpasswort
+$dbname = "haustierverwaltung"; // Der Name deiner Datenbank
+
+// Verbindung erstellen
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verbindung prüfen
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+}
+
+?>
